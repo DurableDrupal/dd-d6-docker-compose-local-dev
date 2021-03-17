3ce6253f5d4b6befadd271785b4d50797829bf9d
Victor Kane <victorkane@gmail.com>
2021-03-17 15:18:30 -0300

    feat(scripts): users inventory script encodes users array to json to make it easily importable for php and js migration scripts

--- drupal-6.38/scripts/dd-get-users.php
+++ drupal-6.38/scripts/dd-get-users.php
@@ -1,7 +1,14 @@
 #!/usr/local/bin/drush
 
+$the_users = array();
 $result = db_query('SELECT uid FROM users');
 while ($obj = db_fetch_object($result)) {
   $user = user_load($obj->uid);
-  print_r($user);
+  // print_r($user);
+  array_push($the_users, $user);
 }
+// print_r($the_users);
+// problematic
+// file_put_contents('scripts/data/inv-users.txt',  '<?php return ' . var_export($the_users, true) . ';');
+// use json instead, more useful too
+file_put_contents('scripts/data/inv-users.txt',  json_encode($the_users));
